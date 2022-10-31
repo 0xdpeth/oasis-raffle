f1740874971c5984ed29579b4b7732eacced46b7
dp <dp@Bubs-MacBook-Pro.local>
2022-10-31 11:22:49 -0700

    re-deploy to gorli testnet

--- deploy.js
+++ deploy.js
@@ -1,13 +1,15 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+const fs = require('fs');
+const config = require('dotenv').config()
+const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  mnemonic,
+  config.parsed.TESTNET_NETWORK_RPC  // infura endpoint
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
